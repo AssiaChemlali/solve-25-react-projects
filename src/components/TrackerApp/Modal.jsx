6c3b8bf58beeaeb4c975e726fdbcd1b2d74b15de
AssiaChemlali <chemlali.assia@gmail.com>
2024-12-07 17:54:45 +0100

    update Tracker App

---
+++ src/components/TrackerApp/Modal.jsx
@@ -0,0 +1,81 @@
+import React, { forwardRef, useRef, useState } from 'react'
+
+const Modal = forwardRef(({onTrans},ref) => {
+
+  const refDesc=useRef()
+  const refAmout=useRef()
+  const refIncome=useRef()
+  const refExpense=useRef()
+
+  function handleClose(){
+    ref.current.close()
+  }
+  
+  function handleAdd(){
+    const desc=refDesc.current.value
+    const amout=parseInt(refAmout.current.value)
+    let type;
+
+   if(refIncome.current.checked){
+      type="income"
+   }
+   if(refExpense.current.checked){
+      type="expense"
+  }
+
+    onTrans({desc,amout,type})
+    ref.current.close()
+  }
+  return (
+    <>
+    <div className='z-50 bg-black opacity-1 w-full '></div>
+    <dialog id="" ref={ref} className='bg-white p-4  w-[400px] rounded '>
+
+     <div className='flex mb-5'>
+      <h4 className='font-bold capitalize text-xl'>Add new Transaction</h4>
+      <span className='ml-auto cursor-pointer' onClick={handleClose}>X</span>
+     </div>
+
+     <div className='mb-5'>
+        <p className='font-bold mb-1'>Enter Description</p>
+        <input 
+        ref={refDesc}
+        className='p-2 border rounded border-gray-200'
+        type="text"  
+        placeholder='Enter transcation'/>
+     </div>
+
+
+     <div className=''>
+        <p className='font-bold mb-1'>Enter Amount</p>
+        <input 
+        ref={refAmout}
+        className='p-2 border rounded border-gray-200'
+        type="number"  
+        placeholder='Enter amount'/>
+     </div>
+
+     <div className='flex  gap-2 mt-3'>
+      <input type="radio" name="trans" id="icome" ref={refIncome} />
+      <label htmlFor="income">Income</label>
+      <input type="radio" name="trans" id="expense"  ref={refExpense}/>
+      <label htmlFor="expense">Expense</label>
+     </div>
+
+     <div className='mt-5 flex  flex-row justify-end gap-3'>
+      <button 
+      onClick={handleClose}
+      className='bg-gray-100 text-black px-3 py-2 capitalize rounded hover:bg-blue-200'>cancel</button>
+
+      <button className='bg-gray-100 text-black px-3 py-2 capitalize rounded hover:bg-blue-200'
+      onClick={handleAdd}>add</button>
+     </div>
+     
+
+    </dialog>
+    </>
+   
+  )
+})
+
+export default Modal
--- src/components/TrackerApp/TrackerApp.jsx
+++ src/components/TrackerApp/TrackerApp.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
-
+import React, { useEffect, useRef, useState } from 'react'
+import Modal from './Modal'
+import { PieChart } from '@mui/x-charts'
 const TrackerApp = () => {
+
+  const ref=useRef()
+  const [transaction,setTransaction]=useState(
+    [])
+    const[balance,setBalance]=useState(0)
+   const [TotalIncome,setTotalIncome]=useState(0)
+   const [TotalExpensive,setTotalExpensive]=useState(0)
+
+  function handleClick(){
+     ref.current.showModal()
+  }
+
+
+ function addTransition(trans){
+    setBalance(trans.amout)
+    setTransaction(prevData=>{
+      return [...prevData,trans]
+    })
+    if(trans.type==="income"){
+      const total= TotalIncome + trans.amout
+      setTotalIncome(total)
+    }
+    if(trans.type==="expense"){
+      const total= TotalExpensive + trans.amout
+      setTotalExpensive(total)
+    }
+
+
+ }
+
+
+
   return (
-    <div>
+    <div className='p-10 w-full'>
+      <div className='flex items-center justify-center mb-10'>
+        <h3 className='text-xl font-extrabold'>Expense Tracker</h3>
+        <button 
+        onClick={handleClick}
+        className='bg-blue-500 text-white p-2 ml-auto rounded'>Add New Transaction</button>
+      </div>
+
+
+      <div className='flex flex-row w-full justify-between mb-10 bg-gray-100'>
+        <div className='p-5 '>
+            <h2 className='font-bold'>Balance is : $ {balance}</h2>
+            <h3> Total income: $ {TotalIncome}</h3>
+            <h3>Total Experience: $ {TotalExpensive}</h3>
+        </div>
+
+        <div className='border p-5 '>
+          <PieChart
+            series={[
+                    {
+                      data: [
+                        { id: 0, value: TotalIncome, label: 'income' },
+                        { id: 1, value: TotalExpensive, label: 'Expene' },
+                      ],
+                    },
+                  ]}
+                  width={400}
+                  height={200}
+          />
+        
+        
+        </div>
+      </div>
+
+      <div className='flex flex-row w-full justify-between bg-red-100 p-4'>
+        <ul>
+          <li className='font-bold'>Income  :</li>
+          {
+            transaction.length >0 &&
+            transaction?.map((item,index)=>{
+              if(item.type==="income"){
+                 return(<li key={index}> {item.type} : {item.amout}$</li>)
+              }
+            })
+          }
+        </ul>
+        <ul>
+          <li className='font-bold'>Expense:</li>
+          {
+            transaction.length >0 &&
+            transaction?.map((item,index)=>{
+                if(item.type==="expense")
+                  return(<li key={index}>{item.type} : {item.amout}$</li>)
+              })
+          }
+        </ul>
+      </div>
       
+      <Modal ref={ref} onTrans={addTransition}/>
+
     </div>
   )
 }
